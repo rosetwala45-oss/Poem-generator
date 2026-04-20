9c21e6a9047186861cce25b6f0eacdd459cf7797
rosetwala45-oss <rosetwala45@gmail.com>
2026-04-20 15:37:39 +0200

    API Integration
    Added axios, prompt, context and displayed answer of AI

--- src/script.js
+++ src/script.js
@@ -1,20 +1,31 @@
 // FUNCTIONS
-function generatePoem(event) {
-  event.preventDefault();
-
-  // Displaying poem
-  let poemElement = document.querySelector("#poem");
-  new Typewriter("#poem", {
-    strings: "Women learning to code, taking flight.",
-    autoStart: true,
-    delay: 50,
-    cursor: "|",
-  });
-}
-
 let poemFormElement = document.querySelector("#poem-generator");
 poemFormElement.addEventListener("submit", generatePoem);
 
-// API call
+function generatePoem(event) {
+  event.preventDefault();
+
+  // API call
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "0dc6tacc431o670b15e9a5cb213feec1";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a poem expert that loves to write short poems. Your missions is to generte a 4 line poem in basic HTML.Seperate each line with a <br />. Make sure to follow user instructions.Do not include a title to the poem. Sign the poem with 'SheCodes AI' in bold inside a <strong> tag at the end of the poem";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+// Displaying poem
+function displayPoem(response) {
+  let poemElement = document.querySelector("#poem");
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 40,
+    cursor: "|",
+  });
+}
 
 // Loading message
